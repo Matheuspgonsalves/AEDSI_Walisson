3bd774e3b4087de0cb9617680088aa870eb8d2e8
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2023-08-19 14:01:37 -0300

    Exercicio 2 feito e execucao correta

--- ListaDeExercicios_Sequencial/EX02.c
+++ ListaDeExercicios_Sequencial/EX02.c
@@ -2,8 +2,24 @@
 #include <stdio.h>
 
 int main(){//Funcao principal
-    puts("MEDIA ARITMETICA");
+    //declaracao e inicializacao de variaveis
+    float num1, num2, num3, media = 0;
 
+    puts("MEDIA ARITMETICA");//saida para informar o oobjetivo do programa
 
+    //Instrucao e alocacao dos valores em memoria
+    puts("Insira 3 numeros para realizar a media");
+    puts("Numero 1:");
+    scanf("%f", &num1);
+
+    puts("Numero 2:");
+    scanf("%f", &num2);
+
+    puts("Numero 3:");
+    scanf("%f", &num3);
+
+    media = (num1 + num2 + num3)/3;
+
+    printf("\nA media da soma dos numeros %.2f, %.2f e %.2f = %.2f", num1, num2, num3, media);
     return 0;
 }
